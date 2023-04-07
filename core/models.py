e7226e9eb94eeae069783b5e13e92179692a40bc
Your Name <natanaelforte2gmail.com>
2023-04-07 17:40:55 -0300

    model commit

--- core/models.py
+++ core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    email = models.EmailField('Email')
+    senha = models.DecimalField('Senha',decimal_places=2,max_digits=8)
